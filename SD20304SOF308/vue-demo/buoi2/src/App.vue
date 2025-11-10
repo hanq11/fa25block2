45d17f51f62348e4d7b052c586b1f22b5b7772f2
hanq11 <>
2025-11-10 13:57:17 +0700

    update

---
+++ SD20304SOF308/vue-demo/buoi2/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import HelloVueJS from './components/HelloVueJS.vue';
+</script>
+
+<template>
+  <main>
+    <HelloVueJS></HelloVueJS>
+  </main>
+</template>
---
+++ SD20304SOF308/vue-demo/buoi2/src/components/HelloVueJS.vue
@@ -0,0 +1,18 @@
+<script setup>
+ // Setup - compositon api
+ // khong co chu setup - option api
+ // var - khong nen dung
+ // let - thay doi duoc
+ // const - khong thay duoc hoan toan
+
+ let message = "Hello world"
+
+ const sayHi = () => { // arrow function
+    return "FPOLY Say hi"
+ }
+</script>
+
+<template>
+    <p>{{ message }}</p>
+    <p>{{ sayHi()  }}</p>
+</template>
