1bca11bb06351e12e87f0e65254cda1e0efc1691
hanq11 <>
2025-11-15 11:10:53 +0700

    update

---
+++ SD20306SOF308/buoi2/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import HelloSOF308 from './components/HelloSOF308.vue';
+
+  // var -- khong dung
+  // let
+  // const
+  let message = "Day la thong bao"
+  let chuDo = "color:red"
+
+  const printHello = () => { // es6
+    alert("hello world")
+  }
+</script>
+
+<template>
+  <HelloSOF308></HelloSOF308>
+  <span :style="chuDo">{{ message.split('').reverse().join('') }}</span>
+
+  <button @click="printHello()">Console log</button>
+
+  <p v-if="1 == 2">Day la v-if</p>
+  <p v-show="1 == 2">Day la v-show</p>
+</template>
