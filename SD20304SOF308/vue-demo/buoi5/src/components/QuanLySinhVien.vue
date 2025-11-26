639a09dd046705862dab5d4b84d72b301dcac5ca
hanq11 <>
2025-11-26 13:04:17 +0700

    update

--- SD20304SOF308/vue-demo/buoi5/src/components/QuanLySinhVien.vue
+++ SD20304SOF308/vue-demo/buoi5/src/components/QuanLySinhVien.vue
@@ -1,7 +1,8 @@
 <template>
     <SinhVienForm v-model:sinhVienForm="sinhVien"></SinhVienForm>
     <div class="container text-center">
-        <button class="btn btn-success" @click="themSinhVien()">Them sinh vien</button>
+        <button class="btn btn-success" @click="themSinhVien()" v-if="updateIndex == -1">Them sinh vien</button>
+        <button class="btn btn-warning" @click="updateSinhVien()" v-if="updateIndex != -1">Update sinh vien</button>
     </div>
     <div class="container">
         <div class="row">
@@ -13,14 +14,19 @@
                             <th scope="col">Ten</th>
                             <th scope="col">Tuoi</th>
                             <th scope="col">Dia chi</th>
+                            <th scope="col">Hanh dong</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="sv in listSinhVien" :key="sv.id">
+                        <tr v-for="(sv, index) in listSinhVien" :key="sv.id">
                             <td>{{ sv.id }}</td>
                             <td>{{ sv.ten }}</td>
                             <td>{{ sv.tuoi }}</td>
                             <td>{{ sv.diaChi }}</td>
+                            <td>
+                                <button class="btn btn-success" @click="showDetail(index)">Chi tiet</button>
+                                <button class="btn btn-danger mx-3" @click="deleteSinhVien(index)">Xoa</button>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
@@ -32,6 +38,9 @@
 <script setup>
 import { ref } from 'vue';
 import SinhVienForm from './SinhVienForm.vue';
+
+const updateIndex = ref(-1)
+
 const listSinhVien = ref([
     {
         id: 1,
@@ -51,7 +60,7 @@ const listSinhVien = ref([
         tuoi: 29,
         diaChi: "Bac Ninh"
     }
-])     
+])
 
 const sinhVien = ref({
     id: "",
@@ -64,5 +73,34 @@ const themSinhVien = () => {
     listSinhVien.value.push({
         ...sinhVien.value
     })
+    clearForm()
+}
+
+const showDetail = (index) => {
+    sinhVien.value = {
+        ...listSinhVien.value[index]
+    }
+    updateIndex.value = index
+}
+
+const updateSinhVien = () => {
+    listSinhVien.value[updateIndex.value] = {
+        ...sinhVien.value
+    }
+    updateIndex.value = -1
+    clearForm()
+}
+
+const deleteSinhVien = (index) => {
+    listSinhVien.value.splice(index, 1)
+}
+
+const clearForm = () => {
+    sinhVien.value = {
+        id: "",
+        ten: "",
+        tuoi: "",
+        diaChi: ""
+    }
 }
 </script>
