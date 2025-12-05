5fb7933215397666beb19c52fd4b8d31679e1430
hanq11 <>
2025-12-05 12:50:56 +0700

    update

---
+++ SD20304SOF308/vue-demo/demo-header-footer/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import QuanLySinhVien from './components/QuanLySinhVien.vue';
+</script>
+
+
+<template>
+  <Header></Header>
+  <QuanLySinhVien></QuanLySinhVien>
+  <Footer></Footer>
+</template>
